02cc5257ce744beec3d90e70eb638034a948de09
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2024-03-14 02:51:25 -0300

    ci: Fix @bin/lint & @bin/format hiding errors

--- @bin/format.bash
+++ @bin/format.bash
@@ -22,8 +22,8 @@ source "${ROOT_DIR}/lib/tool/format.bash" # format_with_env
 main() {
   local format_cmd_type=$1
   local error=0
-  find "${ROOT_DIR}" -type f \( -name '*.bash' -o -name '*.sh' \) | grep -v -E '(.shellpack_deps|/gen/)' | format_with_env "${format_cmd_type}" bash || ((error += $?))
-  find "${ROOT_DIR}" -type f -name '*.bats' | format_with_env "${format_cmd_type}" bats || ((error += $?))
+  format_with_env "${format_cmd_type}" bash < <(find "${ROOT_DIR}" -type f \( -name '*.bash' -o -name '*.sh' \)) || ((error += $?))
+  format_with_env "${format_cmd_type}" bats < <(find "${ROOT_DIR}" -type f -name '*.bats') || ((error += $?))
   if ((error > 0)); then
     exit "$error"
   fi
--- @bin/lint.bash
+++ @bin/lint.bash
@@ -10,8 +10,8 @@ source "${ROOT_DIR}/lib/tool/lint.bash" # lint
 
 main() {
   local error=0
-  find "${ROOT_DIR}" -type f \( -name '*.bash' -o -name '*.sh' \) | grep -v -E '(.shellpack_deps|/gen/)' | lint bash || ((error += $?))
-  find "${ROOT_DIR}" -type f -name '*.bats' | lint bats || ((error += $?))
+  lint bash < <(find "${ROOT_DIR}" -type f \( -name '*.bash' -o -name '*.sh' \)) || ((error += $?))
+  lint bats < <(find "${ROOT_DIR}" -type f -name '*.bats') || ((error += $?))
   if ((error > 0)); then
     exit "$error"
   fi
