9a38c93aad63618327c87f45a6fb890e965d219f
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2024-03-15 00:13:09 -0300

    feat(dict): Add comparition and serialization

---
+++ lib/dict.bash
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+dict_equal() {
+  local -n arr1=$1
+  local -n arr2=$2
+
+  # Check if the number of keys in both arrays are the same
+  if [ "${#arr1[@]}" -ne "${#arr2[@]}" ]; then
+    echo "Arrays are not equal (different lengths)"
+    return 1
+  fi
+
+  # Check if all keys and values in arr1 exist in arr2
+  for key in "${!arr1[@]}"; do
+    if [[ ! -v arr2[$key] ]] || [[ "${arr1[$key]}" != "${arr2[$key]}" ]]; then
+      echo "Arrays are not equal (difference found at key: $key)"
+      return 1
+    fi
+  done
+
+  # Check if all keys and values in arr2 exist in arr1
+  for key in "${!arr2[@]}"; do
+    if [[ ! -v arr1[$key] ]] || [[ "${arr2[$key]}" != "${arr1[$key]}" ]]; then
+      echo "Arrays are not equal (difference found at key: $key)"
+      return 1
+    fi
+  done
+
+  echo "Arrays are equal"
+  return 0
+}
+
+# $1 = source varname (contains associative array to be serialized)
+# $2 = target varname (will contain the serialized string)
+serialize_dict() {
+  local -n dict=$1
+  local -n serialized_str=$2
+  local entry_delimiter=$'\x01'     # Start of Heading as entry delimiter
+  local key_value_delimiter=$'\x1E' # Record Separator as key-value delimiter
+  for key in "${!dict[@]}"; do
+    serialized_str+="${key}${key_value_delimiter}${dict[${key}]}${entry_delimiter}"
+  done
+  # Remove the trailing delimiter
+  serialized_str="${serialized_str%"${entry_delimiter}"}"
+}
+
+# $1 = source varname (contains the serialized string)
+# $2 = target varname (will contain associative array deserialized from string)
+deserialize_to_dict() {
+  local -n serialized=$1
+  local -n dict=$2
+  local entry_delimiter=$'\x01'     # Start of Heading as entry delimiter
+  local key_value_delimiter=$'\x1E' # Record Separator as key-value delimiter
+  IFS="$entry_delimiter" read -ra entries <<<"${serialized}"
+  for entry in "${entries[@]}"; do
+    # Split each entry into key and value based on the key-value delimiter
+    IFS="${key_value_delimiter}" read -r key value <<<"${entry}"
+    dict["${key}"]="${value}"
+  done
+}
---
+++ test/dict_test.bats
@@ -0,0 +1,73 @@
+#!/usr/bin/env bats
+
+# detect LIB_DIR - BEGIN
+TEST_DIR="$(cd "$(dirname "${BATS_TEST_FILENAME}")" && pwd -P || exit 1)"
+LIB_DIR="$(cd "${TEST_DIR}/../lib" && pwd -P || exit 1)"
+# detect LIB_DIR - END
+
+# shellcheck source=lib/dict.bash
+source "${LIB_DIR}/dict.bash"
+
+@test "dict_equal - same" {
+  declare -A mydict=(
+    [key1]="value1"
+    [key2]="value2"
+    [key3]="value3"
+  )
+  declare -A mydict2=(
+    [key1]="value1"
+    [key2]="value2"
+    [key3]="value3"
+  )
+  dict_equal mydict mydict2
+}
+
+@test "dict_equal - different length" {
+  bats_require_minimum_version 1.5.0
+  declare -A mydict1=(
+    [key1]="value1"
+    [key2]="value2"
+    [key3]="value3"
+  )
+  declare -A mydict2=(
+    [key1]="value1"
+    [key2]="value2"
+  )
+  run ! dict_equal mydict1 mydict2
+}
+
+@test "dict_equal - different content" {
+  bats_require_minimum_version 1.5.0
+  # shellcheck disable=SC2034
+  declare -A mydict1=(
+    [key1]="value1"
+    [key2]="value2"
+    [key3]="value3"
+  )
+  # shellcheck disable=SC2034
+  declare -A mydict2=(
+    [key1]="value1"
+    [key2]="value2"
+    [key3]="value3-"
+  )
+  run ! dict_equal mydict1 mydict2
+}
+
+@test "normalized_path - remove" {
+  # shellcheck disable=SC2034
+  declare -A mydict=(
+    [key1]="value1"
+    [key2]="value2"
+    [key3]="value3"
+  )
+  # serialize
+  # shellcheck disable=SC2034
+  declare serialized_dict
+  serialize_dict mydict serialized_dict
+  # deserialize
+  # shellcheck disable=SC2034
+  declare -A mydict_restored
+  deserialize_to_dict serialized_dict mydict_restored
+  # verify
+  dict_equal mydict mydict_restored
+}
