7fdf15bc66b596a353206d03dceca5a31da5ecf3
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2024-03-26 23:52:38 -0300

    feat(color): Add `is_color`

--- lib/color.bash
+++ lib/color.bash
@@ -7,3 +7,13 @@ GREEN=$(printf '\033[32m')
 RED=$(printf '\033[31m')
 YELLOW=$(printf '\033[33m')
 NC=$(printf '\033[0m')
+
+# Color enabled by default
+COLOR=${COLOR:-1}
+
+is_color() {
+  case "${COLOR}" in
+    1 | "true") return 0 ;; # true
+    *) return 1 ;;          # false
+  esac
+}
