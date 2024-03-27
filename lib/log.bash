49dfc5167ab01c520c59f80724c760e88d34ff5e
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2024-03-26 23:53:34 -0300

    breaking(log): Change names & reimpl logging fns

--- lib/log.bash
+++ lib/log.bash
@@ -13,20 +13,58 @@ elif [ -z "${_SHELL_GR_DIR}" ]; then
   _SHELL_GR_DIR="${_ROOT_DIR}"
 fi
 # Library Sourcing
-source "${_SHELL_GR_DIR}/lib/color.bash"
+source "${_SHELL_GR_DIR}/lib/color.bash" # NC, RED, YELLOW, is_color
+source "${_SHELL_GR_DIR}/lib/debug.bash" # is_debug
 
-error() {
-  printf "${RED}%s${NC}\n" "${1}"
+# Expected env vars for log functions:
+# COLOR - to enable/disable colors
+# DEBUG - to enable/disable debug logs
+# PREFIX - log prefix
+
+__LOG_PREFIX="${LOG_PREFIX:-}"
+
+# shellcheck disable=SC2059
+log_error_f() {
+  if is_color; then
+    printf "${RED}${__LOG_PREFIX}${1}${NC}" "${@:2}"
+  else
+    printf "$@"
+  fi
 }
 
-warning() {
-  printf "${YELLOW}%s${NC}\n" "${1}"
+log_error() {
+  log_error_f "%s\n" "$@"
 }
 
-info() {
-  printf "%s\n" "${1}"
+# shellcheck disable=SC2059
+log_warning_f() {
+  if is_color; then
+    printf "${YELLOW}${__LOG_PREFIX}${1}${NC}" "${@:2}"
+  else
+    printf "$@"
+  fi
 }
 
-debug() {
-  printf "%s\n" "${1}"
+log_warning() {
+  log_warning_f "%s\n" "$@"
+}
+
+log_info_f() {
+  # shellcheck disable=SC2059
+  printf "${__LOG_PREFIX}${1}" "${@:2}"
+}
+
+log_info() {
+  log_info_f "${__LOG_PREFIX}%s\n" "$@"
+}
+
+log_debug_f() {
+  if is_debug; then
+    # shellcheck disable=SC2059
+    printf "${__LOG_PREFIX}${1}" "${@:2}"
+  fi
+}
+
+log_debug() {
+  log_debug_f "%s\n" "$@"
 }
