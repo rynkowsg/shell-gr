ca0a7feae860c2528fa84ec5888f4a8fadb5c4a8
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2024-03-28 11:46:10 -0300

    feat(bats_assert): Add bats_assert wrapper
    It only defined bats_assert source lines in one place.

---
+++ lib/bats_assert.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Copyright (c) 2024. All rights reserved.
+# License: MIT License
+
+# Path Initialization
+if [ -n "${SHELL_GR_DIR}" ]; then
+  _SHELL_GR_DIR="${SHELL_GR_DIR}"
+elif [ -z "${_SHELL_GR_DIR}" ]; then
+  _SCRIPT_PATH_1="${BASH_SOURCE[0]:-$0}"
+  _SCRIPT_PATH="$([[ ! "${_SCRIPT_PATH_1}" =~ /bash$ ]] && readlink -f "${_SCRIPT_PATH_1}" || exit 1)"
+  _SCRIPT_DIR="$(cd "$(dirname "${_SCRIPT_PATH}")" && pwd -P || exit 1)"
+  _ROOT_DIR="$(cd "${_SCRIPT_DIR}/.." && pwd -P || exit 1)"
+  _SHELL_GR_DIR="${_ROOT_DIR}"
+fi
+# Library Sourcing
+source "${_SHELL_GR_DIR}/.github_deps/ztombol/bats-assert@v0.3.0/src/assert.bash"
+source "${_SHELL_GR_DIR}/.github_deps/ztombol/bats-support@v0.3.0/src/error.bash"
+source "${_SHELL_GR_DIR}/.github_deps/ztombol/bats-support@v0.3.0/src/lang.bash"
+source "${_SHELL_GR_DIR}/.github_deps/ztombol/bats-support@v0.3.0/src/output.bash"
